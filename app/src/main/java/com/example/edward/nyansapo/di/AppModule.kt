d5df026cb4f52ddfd18735ee0a7a90bb82415f6b
JusticeEli <justelonelin@gmail.com>
2021-03-04 17:46:07 +0300

    implemented student individual page screen

---
+++ app/src/main/java/com/example/edward/nyansapo/di/AppModule.kt
@@ -0,0 +1,44 @@
+package com.example.edward.nyansapo.di
+/*
+
+import com.example.edward.nyansapo.db.NyansapoDatabase
+
+
+import android.app.Application
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+import javax.inject.Qualifier
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(
+            app: Application,
+            callback: NyansapoDatabase.Callback
+    ) = Room.databaseBuilder(app, NyansapoDatabase::class.java, "task_database")
+            .fallbackToDestructiveMigration()
+            .addCallback(callback)
+            .build()
+
+    @Provides
+    fun provideTaskDao(db: NyansapoDatabase) = db.taskDao()
+
+    @ApplicationScope
+    @Provides
+    @Singleton
+    fun provideApplicationScope() = CoroutineScope(SupervisorJob())
+}
+
+@Retention(AnnotationRetention.RUNTIME)
+@Qualifier
+annotation class ApplicationScope
+*/
---
+++ app/src/main/java/com/example/edward/nyansapo/presentation/ui/student/IndividualStudentPageActivity.kt
@@ -0,0 +1,40 @@
+package com.example.edward.nyansapo.presentation.ui.student
+
+import android.graphics.Color
+import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
+import androidx.appcompat.app.AppCompatActivity
+import com.example.edward.nyansapo.databinding.ActivityIndividualStudentPageBinding
+
+
+class IndividualStudentPageActivity : AppCompatActivity() {
+    lateinit var binding: ActivityIndividualStudentPageBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityIndividualStudentPageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setParagraphSpannableString()
+    }
+
+    private fun setParagraphSpannableString() {
+
+
+        val wordtoSpan: Spannable = SpannableString("One morning, the cow went to the lion. She\n" +
+                "wanted him to help her. The lion roared at them.\n" +
+                "The cow and her calf ran away. They found a\n" +
+                "man outside his house. The man loved the\n" +
+                "animals. He made a cow shed for them. The cow\n" +
+                "never went back to the forest.")
+
+        wordtoSpan.setSpan(ForegroundColorSpan(Color.RED), 15, 30, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        wordtoSpan.setSpan(ForegroundColorSpan(Color.RED), 50, 60, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        wordtoSpan.setSpan(ForegroundColorSpan(Color.RED), 90,100, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        wordtoSpan.setSpan(ForegroundColorSpan(Color.RED), 150,170, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        binding.paragraphTxtView.text = wordtoSpan
+    }
+}
